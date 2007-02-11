f786b8153e8241d8e3470da7a9eaff77113b9413
Pedro Algarvio <ufs@ufsoft.org>
2007-02-11 00:00:47 +0000

    [svn r6] Fixed a bug on !BittenExtraTrac, I can't supply 2 !ITemplateProvider or !IRequestFilter from the same file, bummer...

--- bittentrac/web_ui.py
+++ bittentrac/web_ui.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # vim: sw=4 ts=4 fenc=utf-8
 # =============================================================================
-# $Id: web_ui.py 4 2007-02-10 23:28:33Z s0undt3ch $
+# $Id: web_ui.py 6 2007-02-11 00:00:47Z s0undt3ch $
 # =============================================================================
 #             $URL: http://bitten.ufsoft.org/svn/BittenExtraTrac/trunk/bittentrac/web_ui.py $
-# $LastChangedDate: 2007-02-10 23:28:33 +0000 (Sat, 10 Feb 2007) $
-#             $Rev: 4 $
+# $LastChangedDate: 2007-02-11 00:00:47 +0000 (Sun, 11 Feb 2007) $
+#             $Rev: 6 $
 #   $LastChangedBy: s0undt3ch $
 # =============================================================================
 # Copyright (C) 2006 Ufsoft.org - Pedro Algarvio <ufs@ufsoft.org>
@@ -18,12 +18,10 @@ from trac.core import *
 from trac.web.chrome import add_script, add_stylesheet, ITemplateProvider
 from trac.web.api import IRequestFilter
 
-
-class BittenTracTemplates(Component):
+class BittenTracChrome(Component):
     implements(ITemplateProvider, IRequestFilter)
 
     # ITemplateProvider methods
-
     def get_htdocs_dirs(self):
         return [(
             'bittentrac',
@@ -33,24 +31,12 @@ class BittenTracTemplates(Component):
     def get_templates_dirs(self):
         return [pkg_resources.resource_filename(__name__, 'templates')]
 
-    # IRequestFilter methods
     def pre_process_request(self, req, handler):
         return handler
 
     def post_process_request(self, req, template, content_type):
         if req.path_info.startswith('/build'):
             add_stylesheet(req, 'bittentrac/bittentrac.css')
-
-
-class BittenTracJS(Component):
-    implements(IRequestFilter)
-
-    # IRequestFilter methods
-    def pre_process_request(self, req, handler):
-        return handler
-
-    def post_process_request(self, req, template, content_type):
-        if req.path_info.startswith('/build'):
             add_script(req, 'bittentrac/jquery-latest.js')
             add_script(req, 'bittentrac/jquery.tablesorter.js')
             add_script(req, 'bittentrac/jquery.hovertip.js')
